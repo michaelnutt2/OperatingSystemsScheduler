20ca4a6f7a18d284e5078eb8cdb925e4f5c912d8
Michael Nutt <michaelnutt2@my.unt.edu>
2019-03-19 11:37:24 -0500

    Created data set, removed unnecessary files for submitting

--- generator.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-i="0"
-while [ $i -lt 50 ]
-do
-  ./a.out > processes/output$i
-  i=$[$i+1]
-done
--- testing/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-FILES="../test_processes/*"
-
-for file in $FILES;
-do
-  ./a.out $file
-done
